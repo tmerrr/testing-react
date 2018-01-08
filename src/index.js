b8cee9e74a502bcc4fec60cfa3056ec4ff63405a
Lewis <lewisyoul@gmail.com>
2018-01-08 16:06:06 +0000

    feature tests and unit tests working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CheckboxWithLabel from './CheckBoxWithLabel'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CheckboxWithLabel labelOn="On" labelOff="Off" />, document.getElementById('root'));
 registerServiceWorker();
